ed5a000e84217d36d942080a4154bd7d6c260b08
Indrajith K L <mac91112@gmail.com>
2022-06-13 01:27:51 +0530

    Adds Cover Image * Release Command Changes * Embedded a cover image utilizing embedded resource feature

--- release.ps1
+++ release.ps1
@@ -3,5 +3,5 @@ if (Test-Path -Path $Folder){
     Remove-Item -LiteralPath .\game\ -Force -Recurse
 }
 
-dialogc -t aa story.dg stdlib.dg
+dialogc -t aa story.dg stdlib.dg -c .\cover.png -a "The Game"
 aambundle -o game story.aastory
